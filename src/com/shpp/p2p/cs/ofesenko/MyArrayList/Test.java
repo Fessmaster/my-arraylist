33dba1f87a176ee21668dac150350236a9d7860c
Fessmaster <thefessmaster@gmail.com>
2025-08-04 18:48:53 +0300

    Add methods: add () resizeArray() getCapacity() print() toString()

--- src/com/shpp/p2p/cs/ofesenko/MyArrayList/Test.java
+++ src/com/shpp/p2p/cs/ofesenko/MyArrayList/Test.java
@@ -2,12 +2,21 @@ package com.shpp.p2p.cs.ofesenko.MyArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        MyArrayList<Integer> numbers = new MyArrayList<>(10);
+        MyArrayList<Integer> numbers = new MyArrayList<>(3);
         numbers.add(5);
-        numbers.add(11);
         numbers.add(2);
-        numbers.add(3);
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(1);
+        numbers.add(7);
 
         numbers.print();
+        System.out.println();
+        System.out.println("Size: "+numbers.size());
+        System.out.println("Capacity: "+numbers.getCapacity());
+
+
+
     }
 }
