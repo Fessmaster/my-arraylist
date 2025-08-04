855dcfaa1634a0bc4d49d99320d210b149d645d8
Fessmaster <thefessmaster@gmail.com>
2025-08-04 18:48:53 +0300

    Change gitignore

--- src/com/shpp/p2p/cs/ofesenko/MyArrayList/MyArrayList.java
+++ src/com/shpp/p2p/cs/ofesenko/MyArrayList/MyArrayList.java
@@ -1,30 +1,61 @@
 package com.shpp.p2p.cs.ofesenko.MyArrayList;
 
-
 import java.util.Iterator;
 
 
-public class MyArrayList<T> implements Iterable{
-    private final int CAPACITY = 10;
-    private int size=0;
+public class MyArrayList<T> implements Iterable {
+    private int capacity = 10;
+    private int size = 0;
     private Object[] array;
+
+    public MyArrayList() {
+        this.array = new Object[capacity];
+    }
 
     public MyArrayList(int arraySize) {
         this.array = new Object[arraySize];
+        this.capacity = arraySize;
+
     }
 
-    public void add (T elem){
-        if (size == CAPACITY) {
-//            addCapacity()
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    public void add(T elem) {
+        if (size == capacity) {
+            array = resizeArray();
         }
-        array[size]=elem;
+        array[size] = elem;
         size++;
     }
 
-    public void print(){
-        for (int i=0; i<array.length; i++){
-            System.out.println(array[i]);
+    private Object[] resizeArray() {
+        capacity *= 2;
+        Object[] newArray = new Object[capacity];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        return newArray;
+    }
+
+    public void print() {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null)
+                System.out.print(array[i] + " ");
         }
+    }
+
+    public String toString() {
+        String allElements = "";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null)
+                allElements = (i != array.length - 1) ? allElements + array[i] + " " : allElements + array[i];
+        }
+        return ("[" + allElements + "]");
     }
 
     /**
