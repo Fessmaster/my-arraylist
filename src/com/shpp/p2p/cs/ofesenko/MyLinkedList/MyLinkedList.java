4f05da1eaed2871fc7f6d9105ea9c6e95f8b29e1
Fessmaster <thefessmaster@gmail.com>
2025-08-08 11:32:08 +0300

    Begin create MyLinkedList

--- src/com/shpp/p2p/cs/ofesenko/MyLinkedList/MyLinkedList.java
+++ src/com/shpp/p2p/cs/ofesenko/MyLinkedList/MyLinkedList.java
@@ -1,4 +1,21 @@
 package com.shpp.p2p.cs.ofesenko.MyLinkedList;
 
-public class MyLinkedList {
+public class MyLinkedList<T> {
+    private int size = 0;
+    private T element;
+    private MyLinkedList previous = null;
+    private MyLinkedList current = null;
+    private MyLinkedList next = null;
+
+
+    public void add(T element) {
+        if (size > 0) this.previous = curret;
+        this.current=this;
+        this.element = element;
+
+    }
+
+    private class Node <T>{
+        private T element;
+    }
 }
